37229832b0fc3c0e28237d849f3af6a0caff3188
AidanInceer <aidaninceer0@gmail.com>
2023-10-01 14:33:53 +0100

    add webpage title components

--- src/components/navbar/title.jsx
+++ src/components/navbar/title.jsx
@@ -1,6 +1,6 @@
 const Title = () => {
     return (
-        <div id="Title" className="fixed top-0 left-0 h-16 w-screen m-0 flex flex-col bg-black text-white shadow-lg"><h1>AI Photography</h1>
+        <div id="Title" className="fixed top-0 left-0 h-16 w-screen m-0 flex flex-col bg-black text-white shadow-lg"><h1></h1>
         </div>
     )
 }
