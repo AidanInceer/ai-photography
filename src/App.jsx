aff2b302e8aa12018eb8d17001d5038e72fa04b6
AidanInceer <aidaninceer0@gmail.com>
2023-10-05 20:53:02 +0100

    remove hard coded template

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import NavBar from "./components/navbar/Navbar";
+import FramedPicture from "./components/frame/picture";
 
 function App() {
   return (
@@ -7,70 +8,9 @@ function App() {
       <div>
         <NavBar />
       </div>
-      <br />
-      <br />
-      <table className="w-90 left-0">
-        <tr>
-          <td>
-            <img
-              src="./src/static/img/1.jpg"
-              className="p-1 shadow-lg rounded-lg hover:bg-gray-100"
-            ></img>
-          </td>
-          <td>
-            <img
-              src="./src/static/img/2.jpg"
-              className="p-1 shadow-lg rounded-lg hover:bg-gray-100"
-            ></img>
-          </td>
-          <td>
-            <img
-              src="./src/static/img/3.jpg"
-              className="p-1 shadow-lg rounded-lg hover:bg-gray-100"
-            ></img>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <img
-              src="./src/static/img/4.jpg"
-              className="p-1 shadow-lg rounded-lg hover:bg-gray-100"
-            ></img>
-          </td>
-          <td>
-            <img
-              src="./src/static/img/5.jpg"
-              className="p-1 shadow-lg rounded-lg hover:bg-gray-100"
-            ></img>
-          </td>
-          <td>
-            <img
-              src="./src/static/img/6.jpg"
-              className="p-1 shadow-lg rounded-lg hover:bg-gray-100"
-            ></img>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <img
-              src="./src/static/img/7.jpg"
-              className="p-1 shadow-lg rounded-lg hover:bg-gray-100"
-            ></img>
-          </td>
-          <td>
-            <img
-              src="./src/static/img/8.jpg"
-              className="p-1 shadow-lg rounded-lg hover:bg-gray-100"
-            ></img>
-          </td>
-          <td>
-            <img
-              src="./src/static/img/4.jpg"
-              className="p-1 shadow-lg rounded-lg hover:bg-gray-100"
-            ></img>
-          </td>
-        </tr>
-      </table>
+      <div>
+        <FramedPicture />
+      </div>
     </>
   );
 }
