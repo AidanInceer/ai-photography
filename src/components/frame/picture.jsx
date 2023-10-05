2040476ae95926acff7f38566ce6193f8f3f5ccd
AidanInceer <aidaninceer0@gmail.com>
2023-10-05 20:53:17 +0100

    add mvp picture component

---
+++ src/components/frame/picture.jsx
@@ -0,0 +1,14 @@
+const FramedPicture = () => {
+  return (
+    <>
+      <div className="flex">
+        <img
+          src="./src/static/img/1.jpg"
+          className="p-1 shadow-lg rounded-lg hover:bg-gray-100 flex"
+        ></img>
+      </div>
+    </>
+  );
+};
+
+export default FramedPicture;
