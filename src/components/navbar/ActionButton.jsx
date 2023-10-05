f9a020ae1c714c33eb85d94ecb5c14bb59526f63
AidanInceer <aidaninceer0@gmail.com>
2023-10-05 20:53:55 +0100

    reformat action button

--- src/components/navbar/ActionButton.jsx
+++ src/components/navbar/ActionButton.jsx
@@ -2,10 +2,7 @@ const ActionButton = () => {
   return (
     <>
       <div className="flex justify-end">
-        <button
-          className=" m-2 w-32 
-        bg-gray-500 self-auto text-white hover:border-2"
-        >
+        <button className="m-2 w-32 bg-gray-500 self-auto text-white hover:border-2">
           Buy a Print
         </button>
       </div>
