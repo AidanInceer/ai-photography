9874f4eb7f38640a9582732f7076f8867c58717b
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2023-10-06 17:54:40 +0100

    refactor: use tailwind for button

--- src/components/navbar/ActionButton.jsx
+++ src/components/navbar/ActionButton.jsx
@@ -2,7 +2,7 @@ const ActionButton = () => {
   return (
     <>
       <div className="flex justify-end">
-        <button className="m-2 w-32 bg-gray-500 self-auto text-white hover:border-2">
+        <button className="m-2 bg-gray-500 text-white hover:border-2 rounded-s p-2 font-semibold">
           Buy a Print
         </button>
       </div>
