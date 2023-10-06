56b9db33fd8c2a81b8fe04f388c817841081c1c1
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2023-10-06 17:22:55 +0100

    refactor: use functional component

--- src/components/gallery/frame/picture.jsx
+++ src/components/gallery/frame/picture.jsx
@@ -1,32 +1,27 @@
-import React from "react";
+const PictureFrame = ({ filePaths }) => {
+  const imagesPerRow = 3;
 
-class PictureFrame extends React.Component {
-  render() {
-    const { filePaths } = this.props;
-    const imagesPerRow = 3;
+  const rows = [];
+  for (let i = 0; i < filePaths.length; i += imagesPerRow) {
+    const rowImages = filePaths.slice(i, i + imagesPerRow);
 
-    const rows = [];
-    for (let i = 0; i < filePaths.length; i += imagesPerRow) {
-      const rowImages = filePaths.slice(i, i + imagesPerRow);
+    const row = (
+      <div key={i} className="flex wrap">
+        {rowImages.map((filePath, index) => (
+          <img
+            key={index}
+            src={filePath}
+            alt={`Image ${index}`}
+            className="p-1 rounded-lg hover:bg-gray-100 flex"
+          />
+        ))}
+      </div>
+    );
 
-      const row = (
-        <div key={i} className="flex wrap">
-          {rowImages.map((filePath, index) => (
-            <img
-              key={index}
-              src={filePath}
-              alt={`Image ${index}`}
-              className="p-1 rounded-lg hover:bg-gray-100 flex"
-            />
-          ))}
-        </div>
-      );
-
-      rows.push(row);
-    }
-
-    return <div className="image-list">{rows}</div>;
+    rows.push(row);
   }
-}
+
+  return <div className="image-list">{rows}</div>;
+};
 
 export default PictureFrame;
