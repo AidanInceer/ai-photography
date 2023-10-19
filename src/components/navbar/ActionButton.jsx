41c775702207df5f5dc24af8e4fa33948b9d9db2
AidanInceer <aidaninceer0@gmail.com>
2023-10-19 21:40:48 +0100

    remove hover border

--- src/components/navbar/ActionButton.jsx
+++ src/components/navbar/ActionButton.jsx
@@ -2,7 +2,7 @@ const ActionButton = () => {
   return (
     <>
       <div className="flex justify-end">
-        <button className="m-2 right-8 absolute bg-gray-500 text-white hover:border-2 p-2 font-semibold rounded">
+        <button className="m-2 right-8 absolute bg-gray-500 text-white p-2 font-semibold rounded">
           Buy a Print
         </button>
       </div>
