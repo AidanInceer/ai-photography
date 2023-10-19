24e82384efcd5f6c5a74fbea695eb1d3861dfff9
AidanInceer <aidaninceer0@gmail.com>
2023-10-19 22:18:50 +0100

    boop

--- src/components/gallery/frame/picture.jsx
+++ src/components/gallery/frame/picture.jsx
@@ -1,15 +1,40 @@
+import React, { useState } from 'react';
+
 const PictureFrame = ({ filePaths }) => {
+  const [enlargedImagePath, setEnlargedImagePath] = useState(null);
+
+  const handleImageClick = (path) => {
+    setEnlargedImagePath(path);
+  };
+
+  const closeEnlarged = () => {
+    setEnlargedImagePath(null);
+  };
+
   return (
-    <div className="image-list flex grow justify-center flex-wrap flex-row">
-      {filePaths.map((path, index) => (
-        <img
-          key={index}
-          src={path}
-          alt={`Image ${index}`}
-          className="p-1 rounded-lg hover:bg-gray-100 flex max-w-lg object-cover"
-        />
-      ))}
-    </div>
+    <>
+      <div className="image-list flex grow justify-center flex-wrap flex-row">
+        {filePaths.map((path, index) => (
+          <img
+            key={index}
+            src={path}
+            alt={`Image ${index}`}
+            className="p-1 rounded-lg hover:bg-gray-100 flex max-w-lg object-cover cursor-pointer"
+            onClick={() => handleImageClick(path)}
+          />
+        ))}
+      </div>
+
+      {enlargedImagePath && (
+        <div className="enlarged-image-container" onClick={closeEnlarged}>
+          <img
+            src={enlargedImagePath}
+            alt="Enlarged Image"
+            className="enlarged-image"
+          />
+        </div>
+      )}
+    </>
   );
 };
 
--- src/components/gallery/gallery.jsx
+++ src/components/gallery/gallery.jsx
@@ -1,23 +1,24 @@
 import PictureFrame from "./frame/picture";
 
+function getFileUrls() {
+  const urls = [];
+  for (let index = 1; index <= 8; index++) {
+    // Use URL so Vite can bundle images: https://vitejs.dev/guide/assets.html
+    urls.push(new URL(`../../static/img/${index}.jpg`, import.meta.url).href);
+  }
+  return urls;
+}
+
 const Gallery = () => {
   return (
     <>
       <div>
-        <PictureFrame filePaths={getFileUrls()} />
+          <PictureFrame filePaths={getFileUrls()} />
       </div>
     </>
   );
 };
 
-function getFileUrls() {
-  const urls = [];
-  for (let index = 1; index <= 9; index++) {
-    // Use URL so Vite can bundle images: https://vitejs.dev/guide/assets.html
-    urls.push(new URL(`../../static/img/${index}.jpg`, import.meta.url).href);
-  }
 
-  return urls;
-}
 
 export default Gallery;
